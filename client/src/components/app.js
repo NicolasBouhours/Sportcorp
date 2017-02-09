b1c12ca2f59a0966187618abbf78005c73f40c17
NicolasBouhours <nico53960@hotmail.fr>
2017-02-09 23:12:49 +0100

    Adding client basic auth

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <div>
         <Header />
-        <p>React Simple Starter</p>
+        {this.props.children}
       </div>
     );
   }
